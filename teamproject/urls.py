a13944be2fd01f2fd686b457a18453a271836b8f
Liyu <2442176L@student.gla.ac.uk>
2020-02-26 12:36:56 +0000

    second

--- teamproject/urls.py
+++ teamproject/urls.py
@@ -20,6 +20,7 @@ from wonderland import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
     path('wonderland/', include('wonderland.urls')),
     path('admin/', admin.site.urls),
 ]
